3a3585302b6a4968a0aba1c836214b49b85a15b4
Magda <>
2023-01-10 22:00:49 -0500

    toggle button

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 const input = document.getElementById('value');
 const view = document.getElementById('view');
-//const track = document.getElementById("input")
-const views = [ 10, 50, 100, 500, 1 ];
+const cost = document.getElementById('price');
+const toggleButton = document.querySelector('.main__content__billing__ofandon');
+const toggleSwitch = document.querySelector('.main__content__billing__ofandon__circle');
+const views = [
+	{
+		view: '10K pageviews',
+		price: 8.0
+	},
+	{
+		view: '50K pageviews',
+		price: 10.0
+	},
+	{
+		view: '100K pageviews',
+		price: 16.0
+	},
+	{
+		view: '500K pageviews',
+		price: 24.0
+	},
+	{
+		view: '1M pageviews',
+		price: 36.0
+	}
+];
 
 const sliderTrackHandler = () => {
 	const value = input.value * 25;
 	const color = `linear-gradient(90deg, rgb(164, 243, 235) ${value}%, rgb(236, 240, 251) ${value}% )`;
 	input.style.background = color;
-	console.log(value);
 };
 
 input.oninput = () => {
-	console.log(input);
-	//const valuee = input.value;
-	view.innerHTML = views[input.value];
-	//const color = `linear-gradient(90deg, rgb(164, 243, 235)  ${valuee}, rgb(236, 240, 251) 100%- ${value}%);`
-	//input.style.background = color;
+	view.innerHTML = views[input.value].view.toUpperCase();
+	cost.innerHTML = views[input.value].price.toFixed(2);
+};
+
+toggleHandler = () => {
+	if (toggleSwitch.classList.contains('main__content__billing__ofandon__circle')) {
+		toggleSwitch.classList.remove('main__content__billing__ofandon__circle');
+		toggleSwitch.classList.add('main__content__billing__ofandon__circle__toggle');
+	} else {
+		toggleSwitch.classList.add('main__content__billing__ofandon__circle');
+		toggleSwitch.classList.remove('main__content__billing__ofandon__circle__toggle');
+	}
 };
 
 input.oninput();
 input.addEventListener('mousemove', sliderTrackHandler);
+toggleButton.addEventListener('click', toggleHandler);
